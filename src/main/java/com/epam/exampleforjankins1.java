ac01500fe97a5ebc5b35febd4582ceb73cb25623
Oleksandr_Kokhanyuk <oleksandr_kokhanyuk@epam.com>
2016-08-09 17:04:02 +0300

    first commit

---
+++ src/main/java/com/epam/exampleforjankins1.java
@@ -0,0 +1,13 @@
+package com.epam;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Created by Oleksandr_Kokhanyuk on 8/9/2016.
+ */
+public class exampleforjankins1 {
+    public static void main(String[] args) {
+        Logger logger = Logger.getLogger(exampleforjankins1.class);
+        logger.info("Hello");
+    }
+}
---
+++ src/test/java/com/epam/exampleforjankins1Test.java
@@ -0,0 +1,17 @@
+package com.epam;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Oleksandr_Kokhanyuk on 8/9/2016.
+ */
+public class exampleforjankins1Test {
+
+
+    @Test
+    public void testMain() throws Exception {
+        assert (true);
+    }
+}
