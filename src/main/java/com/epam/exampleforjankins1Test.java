ae185864ec1967dc417af8d11493c1acf402b82e
Oleksandr_Kokhanyuk <oleksandr_kokhanyuk@epam.com>
2016-08-15 18:27:07 +0300

    vm2

---
+++ src/main/java/com/epam/exampleforjankins1Test.java
@@ -0,0 +1,16 @@
+package com.epam;
+import java.util.Random;
+
+
+/**
+ * Created by Oleksandr_Kokhanyuk on 8/15/2016.
+ */
+public class exampleforjankins1Test {
+
+    @org.junit.Test
+    public void testMain() throws Exception {
+        Random random = new Random();
+       // assert (random.nextBoolean());
+        assert (true);
+    }
+}
